3e8fc002feed18463d2158fff85f5ae6befc1b7b
pbzweihander <pbzweihander@gmail.com>
2023-07-19 19:23:14 +0900

    yuri.garden custom

--- packages/frontend/src/scripts/theme.ts
+++ packages/frontend/src/scripts/theme.ts
@@ -34,6 +34,7 @@ export const getBuiltinThemes = () => Promise.all(
 		'l-cherry',
 		'l-sushi',
 		'l-u0',
+		'l-birdsite',
 
 		'd-dark',
 		'd-persimmon',
@@ -45,6 +46,7 @@ export const getBuiltinThemes = () => Promise.all(
 		'd-cherry',
 		'd-ice',
 		'd-u0',
+		'd-birdsite',
 	].map(name => import(`../themes/${name}.json5`).then(({ default: _default }): Theme => _default)),
 );
 
