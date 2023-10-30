5592779eae34740e47635973d6c95deb1e656679
pbzweihander <pbzweihander@gmail.com>
2023-10-31 03:11:12 +0900

    Implement OIDC

--- packages/backend/src/server/oauth/OAuth2ProviderService.ts
+++ packages/backend/src/server/oauth/OAuth2ProviderService.ts
@@ -3,12 +3,8 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import dns from 'node:dns/promises';
 import { fileURLToPath } from 'node:url';
 import { Inject, Injectable } from '@nestjs/common';
-import { JSDOM } from 'jsdom';
-import httpLinkHeader from 'http-link-header';
-import ipaddr from 'ipaddr.js';
 import oauth2orize, { type OAuth2, AuthorizationError, ValidateFunctionArity2, OAuth2Req, MiddlewareRequest } from 'oauth2orize';
 import oauth2Pkce from 'oauth2orize-pkce';
 import fastifyView from '@fastify/view';
@@ -16,7 +12,6 @@ import pug from 'pug';
 import bodyParser from 'body-parser';
 import fastifyExpress from '@fastify/express';
 import { verifyChallenge } from 'pkce-challenge';
-import { mf2 } from 'microformats-parser';
 import { secureRndstr } from '@/misc/secure-rndstr.js';
 import { HttpRequestService } from '@/core/HttpRequestService.js';
 import { kinds } from '@/misc/api-permissions.js';
@@ -30,7 +25,6 @@ import type { MiLocalUser } from '@/models/User.js';
 import { MemoryKVCache } from '@/misc/cache.js';
 import { LoggerService } from '@/core/LoggerService.js';
 import Logger from '@/logger.js';
-import { StatusError } from '@/misc/status-error.js';
 import type { ServerResponse } from 'node:http';
 import type { FastifyInstance } from 'fastify';
 
@@ -38,113 +32,9 @@ import type { FastifyInstance } from 'fastify';
 // https://github.com/node-oauth/node-oauth2-server/issues/180 is figured out.
 // Upstream the various validations and RFC9207 implementation in that case.
 
-// Follows https://indieauth.spec.indieweb.org/#client-identifier
-// This is also mostly similar to https://developers.google.com/identity/protocols/oauth2/web-server#uri-validation
-// although Google has stricter rule.
-function validateClientId(raw: string): URL {
-	// "Clients are identified by a [URL]."
-	const url = ((): URL => {
-		try {
-			return new URL(raw);
-		} catch { throw new AuthorizationError('client_id must be a valid URL', 'invalid_request'); }
-	})();
-
-	// "Client identifier URLs MUST have either an https or http scheme"
-	// But then again:
-	// https://datatracker.ietf.org/doc/html/rfc6749.html#section-3.1.2.1
-	// 'The redirection endpoint SHOULD require the use of TLS as described
-	// in Section 1.6 when the requested response type is "code" or "token"'
-	const allowedProtocols = process.env.NODE_ENV === 'test' ? ['http:', 'https:'] : ['https:'];
-	if (!allowedProtocols.includes(url.protocol)) {
-		throw new AuthorizationError('client_id must be a valid HTTPS URL', 'invalid_request');
-	}
-
-	// "MUST contain a path component (new URL() implicitly adds one)"
-
-	// "MUST NOT contain single-dot or double-dot path segments,"
-	const segments = url.pathname.split('/');
-	if (segments.includes('.') || segments.includes('..')) {
-		throw new AuthorizationError('client_id must not contain dot path segments', 'invalid_request');
-	}
-
-	// ("MAY contain a query string component")
-
-	// "MUST NOT contain a fragment component"
-	if (url.hash) {
-		throw new AuthorizationError('client_id must not contain a fragment component', 'invalid_request');
-	}
-
-	// "MUST NOT contain a username or password component"
-	if (url.username || url.password) {
-		throw new AuthorizationError('client_id must not contain a username or a password', 'invalid_request');
-	}
-
-	// ("MAY contain a port")
-
-	// "host names MUST be domain names or a loopback interface and MUST NOT be
-	// IPv4 or IPv6 addresses except for IPv4 127.0.0.1 or IPv6 [::1]."
-	if (!url.hostname.match(/\.\w+$/) && !['localhost', '127.0.0.1', '[::1]'].includes(url.hostname)) {
-		throw new AuthorizationError('client_id must have a domain name as a host name', 'invalid_request');
-	}
-
-	return url;
-}
-
 interface ClientInformation {
 	id: string;
-	redirectUris: string[];
-	name: string;
-}
-
-// https://indieauth.spec.indieweb.org/#client-information-discovery
-// "Authorization servers SHOULD support parsing the [h-app] Microformat from the client_id,
-// and if there is an [h-app] with a url property matching the client_id URL,
-// then it should use the name and icon and display them on the authorization prompt."
-// (But we don't display any icon for now)
-// https://indieauth.spec.indieweb.org/#redirect-url
-// "The client SHOULD publish one or more <link> tags or Link HTTP headers with a rel attribute
-// of redirect_uri at the client_id URL.
-// Authorization endpoints verifying that a redirect_uri is allowed for use by a client MUST
-// look for an exact match of the given redirect_uri in the request against the list of
-// redirect_uris discovered after resolving any relative URLs."
-async function discoverClientInformation(logger: Logger, httpRequestService: HttpRequestService, id: string): Promise<ClientInformation> {
-	try {
-		const res = await httpRequestService.send(id);
-		const redirectUris: string[] = [];
-
-		const linkHeader = res.headers.get('link');
-		if (linkHeader) {
-			redirectUris.push(...httpLinkHeader.parse(linkHeader).get('rel', 'redirect_uri').map(r => r.uri));
-		}
-
-		const text = await res.text();
-		const fragment = JSDOM.fragment(text);
-
-		redirectUris.push(...[...fragment.querySelectorAll<HTMLLinkElement>('link[rel=redirect_uri][href]')].map(el => el.href));
-
-		let name = id;
-		if (text) {
-			const microformats = mf2(text, { baseUrl: res.url });
-			const nameProperty = microformats.items.find(item => item.type?.includes('h-app') && item.properties.url.includes(id))?.properties.name[0];
-			if (typeof nameProperty === 'string') {
-				name = nameProperty;
-			}
-		}
-
-		return {
-			id,
-			redirectUris: redirectUris.map(uri => new URL(uri, res.url).toString()),
-			name: typeof name === 'string' ? name : id,
-		};
-	} catch (err) {
-		console.error(err);
-		logger.error('Error while fetching client information', { err });
-		if (err instanceof StatusError) {
-			throw new AuthorizationError('Failed to fetch client information', 'invalid_request');
-		} else {
-			throw new AuthorizationError('Failed to parse client information', 'server_error');
-		}
-	}
+	redirectUri: string;
 }
 
 type OmitFirstElement<T extends unknown[]> = T extends [unknown, ...(infer R)]
@@ -236,7 +126,7 @@ export class OAuth2ProviderService {
 		private config: Config,
 		private httpRequestService: HttpRequestService,
 		@Inject(DI.accessTokensRepository)
-		accessTokensRepository: AccessTokensRepository,
+		private accessTokensRepository: AccessTokensRepository,
 		idService: IdService,
 		@Inject(DI.usersRepository)
 		private usersRepository: UsersRepository,
@@ -350,9 +240,7 @@ export class OAuth2ProviderService {
 
 	@bindThis
 	public async createServer(fastify: FastifyInstance): Promise<void> {
-		// https://datatracker.ietf.org/doc/html/rfc8414.html
-		// https://indieauth.spec.indieweb.org/#indieauth-server-metadata
-		fastify.get('/.well-known/oauth-authorization-server', async (_request, reply) => {
+		fastify.get('/.well-known/openid-configuration', async (_request, reply) => {
 			reply.send({
 				issuer: this.config.url,
 				authorization_endpoint: new URL('/oauth/authorize', this.config.url),
@@ -383,6 +271,37 @@ export class OAuth2ProviderService {
 		});
 		fastify.post('/oauth/decision', async () => { });
 		fastify.post('/oauth/token', async () => { });
+		fastify.get('/oauth/userinfo', async (request, reply) => {
+			const token = request.headers.authorization?.startsWith('Bearer ') ? request.headers.authorization.slice(7) : undefined;
+			if (token != null) {
+				const accessToken = await this.accessTokensRepository.findOneBy({ token: token });
+				if (accessToken != null) {
+					this.accessTokensRepository.update(accessToken.id, { lastUsedAt: new Date() });
+
+					const user = await this.cacheService.localUserByIdCache.fetch(
+						accessToken.userId,
+						() => this.usersRepository.findOneBy({ id: accessToken.userId }) as Promise<MiLocalUser>,
+					);
+
+					reply.send({
+						sub: user.id,
+						name: user.username,
+						preferred_username: user.name,
+					});
+					return;
+				}
+			}
+
+			reply.code(401);
+			reply.send({
+				error: {
+					message: 'Unauthorized.',
+					code: 'UNAUTHORIZED',
+					id: 'f3034c73-8b51-4a69-85f8-423fb0bb2460',
+					kind: 'client',
+				},
+			});
+		});
 
 		fastify.register(fastifyView, {
 			root: fileURLToPath(new URL('../web/views', import.meta.url)),
@@ -403,27 +322,7 @@ export class OAuth2ProviderService {
 
 				this.#logger.info(`Validating authorization parameters, with client_id: ${clientID}, redirect_uri: ${redirectURI}, scope: ${scope}`);
 
-				const clientUrl = validateClientId(clientID);
-
-				// https://indieauth.spec.indieweb.org/#client-information-discovery
-				// "the server may want to resolve the domain name first and avoid fetching the document
-				// if the IP address is within the loopback range defined by [RFC5735]
-				// or any other implementation-specific internal IP address."
-				if (process.env.NODE_ENV !== 'test' || process.env.MISSKEY_TEST_CHECK_IP_RANGE === '1') {
-					const lookup = await dns.lookup(clientUrl.hostname);
-					if (ipaddr.parse(lookup.address).range() !== 'unicast') {
-						throw new AuthorizationError('client_id resolves to disallowed IP range.', 'invalid_request');
-					}
-				}
-
-				// Find client information from the remote.
-				const clientInfo = await discoverClientInformation(this.#logger, this.httpRequestService, clientUrl.href);
-
-				// Require the redirect URI to be included in an explicit list, per
-				// https://datatracker.ietf.org/doc/html/draft-ietf-oauth-security-topics#section-4.1.3
-				if (!clientInfo.redirectUris.includes(redirectURI)) {
-					throw new AuthorizationError('Invalid redirect_uri', 'invalid_request');
-				}
+				const clientInfo: ClientInformation = { id: clientID, redirectUri: redirectURI };
 
 				try {
 					const scopes = [...new Set(scope)].filter(s => kinds.includes(s));
@@ -468,6 +367,8 @@ export class OAuth2ProviderService {
 		fastify.use('/oauth/token', bodyParser.json({ strict: true }));
 		fastify.use('/oauth/token', this.#server.token());
 		fastify.use('/oauth/token', this.#server.errorHandler());
+
+		fastify.use('/oauth/userinfo', this.#server.errorHandler());
 
 		// Return 404 for any unknown paths under /oauth so that clients can know
 		// whether a certain endpoint is supported or not.
